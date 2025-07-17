978a937209853a1d0b257b23dcbbdddfcd073f15
angel <donaldocosme1303@gmail.com>
2025-07-17 11:52:24 -0600

    Se implemento la funcion restar()

--- login.py
+++ login.py
@@ -1,1 +1,10 @@
 print("Hola Git, Buenas noches!!")
+
+# FUNCION RESTA
+def restar(valorUs):
+    restaTotal = valorUs - 10
+    return restaTotal
+
+# CONSTRUCTOR O INICIALIZADOR
+if __name__ == "__main__":
+    restar(5)
